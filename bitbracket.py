dceac5634216929158487196974c2cdd9f74d786
Thomas Kirkpatrick <jtkiv.ee@gmail.com>
2019-03-02 09:52:03 -0500

    Updated docstrings.

--- bitbracket.py
+++ bitbracket.py
@@ -1,44 +1,22 @@
 """
 bitbracket - A simulator for symmetric, single elimination brackets.
 
-bitbracket works independently from team data and winning probability
-functions. This means that you can adapt it easily to your own project.
-
-To use bitbracket, you need a list/tuple of team data, and a probability
-function to determine the likelihood of one team beating another.
-
 There are only three functions to be aware of in bitbracket:
 
     champion(bitbracket, teams)
-        Take a bitbracket and list of team data and return the team data
-        of the champion.
+        Take a bitbracket and list of team objects and return the team
+        object corresponding to the champion.
 
     simulate(teams, p, n=1000)
-        Take a list of team data, a probability function, and a number
-        of iterations and return a collections.Counter with bitbracket
-        keys. Use the most_common() function on the Counter to sort the
-        results or pull the N most common results.
+        Take a list of team objects, a probability function, and a
+        number of iterations and return a collections.Counter with
+        bitbracket keys.
 
     translate(bitbracket, teams)
-        Take a bitbracket and list of team data and return nested lists
-        of teams representing the rounds of the tournament. The first
-        entry contains all teams, the last entry is a list containing
-        only the champion.
-
-Example usage:
-
-    >>> import bitbracket
-    >>> teams = list(range(64))
-    >>> def p(team_1, team_2):
-            return 0.5
-    >>> bbs = bitbracket.simulate(teams, p, n=1000)
-    >>> bb, occur = bbs.most_common(1)[0]
-    >>> champ = bitbracket.champion(bb, teams)
-    >>> pct = occur/1000
-    >>> print('{} wins it all {:.1%} of the time.'.format(champ, pct))
-    >>> bracket = bitbracket.translate(bb, teams)
-    >>> for rnd in bracket:
-            print(rnd)
+        Take a bitbracket and list of team objects and return nested
+        lists of teams representing the rounds of the tournament. The
+        first entry contains all teams, the last entry is a list
+        containing only the champion.
 """
 import collections
 import random
@@ -51,26 +29,19 @@ def champion(bitbracket, teams):
 
 
 def simulate(teams, p, n=1000):
-    """Create bitbrackets for the provided teams.
+    """Simulate the tournament n times, record bitbracket outcomes.
 
-    teams is a list/tuple of team data. This can be whatever team data
-    you have for your application; that may just be a string team name,
-    or it could be an instance of a Team class, etc.
+    Note that the returned Counter object has a most_common() method
+    to sort all by frequency. See python collections docs for more info.
 
-    p is a function input that takes two entries from teams and returns
-    the probability that the first team wins. For example:
+    Args:
+        teams: A list/tuple of team objects.
+        p: A function that takes two team objects and returns the
+            probability that the first team beats the second.
+        n: The number of simulations to run; default is 1000.
 
-        def p(team_1, team_2):
-            return team_2.rank/(team_1.rank + team_2.rank)
-
-    n is the number of simulations to perform.
-
-    Returns a collections.Counter where keys are bitbrackets, and
-    values are the number of times the bitbracket appeared.
-
-    One of the most common post-simulation operations is to sort the
-    brackets by likelihood. This can be done using the .most_common()
-    method on the return Counter object. No need to reinvent the wheel!
+    Returns:
+        A collections.Counter object with bitbracket keys.
     """
     counter = collections.Counter()
 
@@ -82,7 +53,7 @@ def simulate(teams, p, n=1000):
 
 
 def translate(bitbracket, teams):
-    """Convert a bitbracket into a nested lists of teams."""
+    """Convert a bitbracket into nested lists of teams."""
     teams = list(teams)
 
     bracket = [teams.copy()]
@@ -99,7 +70,7 @@ def translate(bitbracket, teams):
 
 
 def _simulation_iteration(teams, p):
-    """Run a single simulation, return a bitbracket."""
+    """Run a single simulation and return a bitbracket."""
     teams = list(teams)
 
     bitbracket = 0
